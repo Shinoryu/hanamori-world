3d3a082138c794246396de416c8694f4863c0352
Shinoryu <41242414+Shinoryu@users.noreply.github.com>
2020-09-26 18:40:08 -0400

    5
    map update

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1600971645",
+        "cacheTag": "1601159704",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "hanamori",
-            "last_rendertime": 1600970098,
+            "last_rendertime": 1601159098,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
@@ -76,7 +76,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "hanamori",
-            "last_rendertime": 1600970098,
+            "last_rendertime": 1601159098,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
