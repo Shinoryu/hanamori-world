d517de71b63febbce3367a8382ec6ed38cdac4cb
Shinoryu <41242414+Shinoryu@users.noreply.github.com>
2020-09-27 14:35:58 -0400

    7
    map update

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1601201825",
+        "cacheTag": "1601231266",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "hanamori",
-            "last_rendertime": 1601200595,
+            "last_rendertime": 1601230231,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
@@ -76,7 +76,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "hanamori",
-            "last_rendertime": 1601200595,
+            "last_rendertime": 1601230231,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
